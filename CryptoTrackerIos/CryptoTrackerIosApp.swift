12946f8439334cb2d06c812cd9b7d1dd6ab7537b
Hammad335 <hamadmemon946@gmail.com>
2024-12-09 15:28:32 +0500

    Initial Commit

---
+++ CryptoTrackerIos/CryptoTrackerIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerIosApp.swift
+//  CryptoTrackerIos
+//
+//  Created by Hamad on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
