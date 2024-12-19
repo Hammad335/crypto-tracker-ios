37af8378b16671f3b3e0a139e32ba539512a5f50
Hammad335 <hamadmemon946@gmail.com>
2024-12-19 08:15:48 +0500

    - search functionality completed - using combine for combining text & coins subscribers - debouncer for efficient searching

--- CryptoTrackerIos/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTrackerIos/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
-    @Published var searchText : String = ""
+    @Published var searchText: String = ""
 
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
@@ -20,10 +20,36 @@ class HomeViewModel: ObservableObject {
     }
 
     func addSubscribers() {
-        dataService.$allCoins
-            .sink {
-                [weak self] coins in
-                self?.allCoins = coins
-            }.store(in: &cancellables)
+        // subscribing to dataService.allCoins publisher
+        // not needed any more; because we're subscribing to this publisher combined with
+        // searchText publisher below
+//        dataService.$allCoins
+//            .sink {
+//                [weak self] coins in
+//                self?.allCoins = coins
+//            }.store(in: &cancellables)
+
+        // subscribing to both publishers i.e. searchText & dataService.allCoins using combine
+        // using debouncer for efficient searching
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] filteredCoins in
+                self?.allCoins = filteredCoins
+            }
+            .store(in: &cancellables)
+    }
+
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else { return coins }
+
+        let lowercassed = text.lowercased()
+
+        return coins.filter { coin in
+            coin.name.lowercased().contains(lowercassed)
+                || coin.symbol.lowercased().contains(lowercassed)
+                || coin.id.lowercased().contains(lowercassed)
+        }
     }
 }
